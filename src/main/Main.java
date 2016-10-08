c19063828de82575115202eece9dfc346fec1874
Anry091 <svjavoronkin-08-30@yandex.ru>
2016-10-09 01:15:54 +0300

    add file

---
+++ src/main/Main.java
@@ -0,0 +1,13 @@
+package main;
+
+/**
+ * Created by Anry on 08.10.2016.
+ */
+public class Main {
+
+    public static void main (String[] args)
+    {
+       System.out.println("Привет  ") ;
+    }
+
+}
